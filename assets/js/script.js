91aa3269d70bddcc5d0cdf95a62c788df0f711c9
Talha <talha18513@gmail.com>
2024-08-17 09:53:53 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const closeBtn = document.querySelector(".mobile-menu .close-btn");
+  const body = document.body;
+
+  function toggleMenu() {
+    mobileMenu.classList.toggle("active");
+    body.classList.toggle("no-scroll");
+  }
+
+  hamburgerMenu.addEventListener("click", toggleMenu);
+  closeBtn.addEventListener("click", toggleMenu);
+});
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+
+  // Hide all tab contents
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the active class from all tab links
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab content and add the active class to the corresponding tab link
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Set the default tab to be open
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".tab-link.active").click();
+});
+
+document.querySelectorAll(".accordion-item-header").forEach((header) => {
+  header.addEventListener("click", () => {
+    const activeItem = document.querySelector(".accordion-item.active");
+    if (activeItem && activeItem !== header.parentElement) {
+      activeItem.classList.remove("active");
+      activeItem.querySelector(".accordion-item-body").style.maxHeight = 0;
+    }
+    const item = header.parentElement;
+    item.classList.toggle("active");
+    const body = header.nextElementSibling;
+    if (item.classList.contains("active")) {
+      body.style.maxHeight = body.scrollHeight + "px";
+    } else {
+      body.style.maxHeight = 0;
+    }
+  });
+});
+
+document.getElementById("learnMoreBtn").addEventListener("click", function () {
+  var moreInfo = document.getElementById("moreInfo");
+  if (moreInfo.classList.contains("visible")) {
+    moreInfo.classList.remove("visible");
+    this.textContent = "Learn More About Char Dham Yatra 2024";
+  } else {
+    moreInfo.classList.add("visible");
+    this.textContent = "Show Less";
+  }
+});
+document.getElementById("learnMoreBtn2").addEventListener("click", function () {
+  var moreInfo2 = document.getElementById("moreInfo2");
+  if (moreInfo2.classList.contains("visible")) {
+    moreInfo2.classList.remove("visible");
+    this.textContent = "Learn More About Char Dham Yatra 2024";
+  } else {
+    moreInfo2.classList.add("visible");
+    this.textContent = "Show Less";
+  }
+});
---
+++ read_me.txt
@@ -0,0 +1,88 @@
+////////////////////////////////////////////////////////
+Read more exapandable content
+
+<button id="learnMoreBtn" class="bold-heading font-red decoration-none">
+    Read More About Me
+</button>
+<div id="moreInfo" class="hidden">
+.....content here .....
+</div>
+
+/////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////
+Slider or carousel
+
+<div class="class-name">
+    <div class="slider-item-class">
+        content goes here
+    </div>
+    <div class="slider-item-class">
+        content goes here
+    </div>
+    <div class="slider-item-class">
+        content goes here
+    </div>
+    <div class="slider-item-class">
+        content goes here
+    </div>
+</div>
+
+
+    <script>
+      $(document).ready(function () {
+        $(".class-name").owlCarousel({
+          loop: true,
+          dots: true,
+          autoplay: true,
+          autoplayTimeout: 5000,
+          autoplayHoverPause: true,
+          responsive: {
+            0: {
+              items: 1,
+            },
+            768: {
+              items: 2,
+            },
+            1000: {
+              items: 4,
+            },
+          },
+        });
+      });
+    </script>
+
+/////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////
+Tabs
+
+<div class="tabs">
+    <button class="tab-link active" onclick="openTab(event, 'Tab-Name')">
+        Tab Name
+    </button>
+    <button class="tab-link" onclick="openTab(event, 'Tab-Name-2')">
+        Tab Name 2
+    </button>
+    <button class="tab-link" onclick="openTab(event, 'Tab-Name-3')">
+        Tab Name 3
+    </button>
+</div>
+
+
+<div id="Tab-Name" class="tab-content">
+    Content goes here
+</div>
+<div id="Tab-Name-2" class="tab-content" style="display: none">
+    Content goes here
+</div>
+<div id="Tab-Name-3" class="tab-content" style="display: none">
+    Content goes here
+</div>
+
+
+/////////////////////////////////////////////////////////
+
